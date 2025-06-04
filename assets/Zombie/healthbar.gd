631d5ae6e9a80d9b7689908adf46b220e89bce53
Soufyan <soyfsoyf20004@gmail.com>
2025-06-04 11:17:30 +0100

    ..

--- assets/Zombie/healthbar.gd
+++ assets/Zombie/healthbar.gd
@@ -1,9 +1,25 @@
 extends Control
 
-@onready var bar = $texture_progress_bar
+@onready var bar: ProgressBar = $texture_progress_bar
 @onready var label = $HealthLabel
+
+@export var anchor_path : NodePath
+@export var offset := Vector3.UP * 0.5
+
+var anchor_node: Node3D
 
 func update_health(current, max):
 	bar.max_value = max
 	bar.value = current
 	label.text = "%d/%d" % [current, max]
+
+func _ready():
+	anchor_node = get_node(anchor_path)
+
+func _process(delta):
+	if anchor_node:
+		var world_position = anchor_node.global_transform.origin + offset
+		var camera = get_viewport().get_camera_3d()
+		if camera:
+			var screen_position = camera.unproject_position(world_position)
+			global_position = screen_position
